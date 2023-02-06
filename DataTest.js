4d7c6e9a79e081dab59e7224c6521949c10fcff3
9ut4 <9ut4@fresnou.org>
2023-02-06 13:41:48 -0800

    initial commit

---
+++ DataTest.js
@@ -0,0 +1,12 @@
+console.log(typeof 15);
+console.log(typeof `this`);
+console.log(typeof "that");
+console.log(typeof true);
+console.log(typeof false);
+
+console.log( 7 == 7 );
+console.log( 7 != 7 );
+console.log( 7 == 10 );
+console.log( 7!= 10 );
+console.log( 15 > 10);
+console.log(15 > 15);
---
+++ DataTypes.js
@@ -0,0 +1,35 @@
+console.log(8);
+
+//This is a cool comment
+
+//Numbers
+//Integers
+console.log(24);
+console.log(25);
+
+//Floating Point Numbers (Float)
+console.log(3.14);
+console.log(3.48);
+
+//Strings
+//Characters, letters,symbols, spaces
+console.log("Hello World");
+console.log('!@#$ is also a string');
+console.log(`backticks by the 1`);
+
+//Arithmetic Operators (Math)
+console.log(14+2);
+console.log(14-2);
+console.log(14*2); //Shift-8 gives *
+console.log(14/2);
+console.log(14%2); //% is called MOD
+//it means remaining after dividing
+
+//Concatenation means to combine strings
+console.log("My name"+" is Mari");
+console.log(`My`+` `+`age`+` `+`is `+15+`.`);
+console.log("Your age is\n16.");
+
+//Template literal - plug in some calculated value
+//Use backticks `` by the `` 1 on your keyboard
+console.log(`15 divided by 10 = ${15/10} asdfsadf`);
